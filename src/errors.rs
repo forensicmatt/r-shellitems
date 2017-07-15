517ff7c16ebd7e64d89514f556e3895817e3d482
matthew seyer <matthew.seyer@gmail.com>
2017-07-15 11:11:00 -0500

    Added UTF errors

--- src/errors.rs
+++ src/errors.rs
@@ -1,3 +1,4 @@
+use std::string::FromUtf8Error;
 use std::fmt;
 use std::fmt::Display;
 use std::io;
@@ -5,6 +6,7 @@ use std::io;
 #[derive(Debug)]
 pub enum ErrorKind {
     IoError,
+    Utf16Error
 }
 
 // Lnk Parsing Error
@@ -17,7 +19,16 @@ pub struct ShellItemError {
     // Any additional information passed along, such as the argument name that caused the error
     pub info: Option<Vec<String>>,
 }
-
+impl ShellItemError {
+    #[allow(dead_code)]
+    pub fn utf16_decode_error(err: String)->Self{
+        ShellItemError {
+            message: format!("{}",err),
+            kind: ErrorKind::Utf16Error,
+            info: Some(vec![]),
+        }
+    }
+}
 impl From<io::Error> for ShellItemError {
     fn from(err: io::Error) -> Self {
         ShellItemError {
@@ -27,7 +38,15 @@ impl From<io::Error> for ShellItemError {
         }
     }
 }
-
+impl From<FromUtf8Error> for ShellItemError {
+    fn from(err: FromUtf8Error) -> Self {
+        ShellItemError {
+            message: format!("{}",err),
+            kind: ErrorKind::Utf16Error,
+            info: None,
+        }
+    }
+}
 impl Display for ShellItemError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { writeln!(f, "{}", self.message) }
 }
